01e5b1bc624d38a121e0dbd21334fa89db1bc1e2
Jatin Ramchandani <jatinramchandani307@gmail.com>
2022-03-12 03:26:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { React, useEffect, useState } from 'react';
+import NewTask from './components/NewTask';
+import Tasks from './components/Tasks/Tasks'
+import './App.css'
+
+const DUMMY_Tasks = [];
+
 
 function App() {
+
+  const [tasks, setTasks] = useState(DUMMY_Tasks);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(response => {
+        return response.json();
+      }).then((data) => {
+        let topdata = [];
+        for (let i = 0; i < 50; i++) {
+          topdata.push(data[i]);
+        }
+        setTasks(topdata)
+        // console.log(data[0]);
+      })
+  }, [])
+
+  const addTaskHandler = (task) => {
+    setTasks((prevTasks) => {
+      return [...prevTasks, task];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='allapp'>
+        <NewTask onAddTask={addTaskHandler} />
+        <Tasks items={tasks} />
+      </div>
     </div>
   );
 }
